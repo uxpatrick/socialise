30e9dde6a6d97149173ebad9ef9765644ef68eab
Patryk <58486920+uxpatrick@users.noreply.github.com>
2023-10-12 14:19:29 +0000

    Pending changes exported from your codespace

---
+++ App/Login/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logowanie</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="container">
+        <div class="container-login">
+
+          <div class="container-login-header">
+            <h2>Skorzystaj z platformy <span class='login-header-span'>sociali.se</span></h2>
+            <h1>Zaloguj się</h1>
+          </div>
+
+          <div class="container-login-inputs">
+            <input type="text" name="login" placeholder="Login"></input>
+            <input type="password" name="password" placeholder="Hasło"></input>
+          </div>
+
+          <div class="container-login-buttons">
+            <button class="login-button">Zaloguj się</button>
+            <h2 class="register-button"><u>Stwórz konto</u></h2>
+          </div>
+
+        </div>
+      </div>
+</body>
+</html>
---
+++ App/Register/register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rejestracja</title>
+    <link rel="stylesheet" href="register.css">
+</head>
+<body>
+    <div class="container">
+        <div class="container-register">
+
+          <div class="container-register-header">
+            <h2>Skorzystaj z platformy <span class='register-header-span'>sociali.se</span></h2>
+            <h1>Zaloguj się</h1>
+          </div>
+
+          <div class="container-register-inputs">
+            <input type="text" name="login" placeholder="Login"></input>
+            <input type="text" name="mail" placeholder="E-mail"></input>
+            <input type="password" name="password" placeholder="Hasło"></input>
+          </div>
+
+          <div class="container-register-buttons">
+            <button class="register-button">Zarejestruj się</button>
+            <h2 class="register-button"><u>Logowanie</u></h2>
+          </div>
+
+        </div>
+      </div>
+</body>
+</html>
